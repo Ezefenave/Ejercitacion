8850cd9eee31858dc8288ec351ed8b067235d746
ezefenave <ezefenave2209@gmail.com>
2024-05-14 15:00:29 -0300

    Orden

--- 4. Ejercicio_unidad5/ejercicio13.c
+++ 4. Ejercicio_unidad5/ejercicio13.c
@@ -2,3 +2,22 @@
 13. Implementar una función que encuentre el máximo común divisor (MCD) de dos números enteros
 ingresados por el usuario.
 */
+
+#include <stdio.h>
+void buscador();
+
+int main(){
+    int n1, n2;
+
+    printf("Ingrese el primer numero: ");
+    scanf("%d",&n1);
+    
+    printf("Ingrese el segundo numero: ");
+    scanf("%d",&n2);
+
+    printf("El MCD de %d y %d es: %d\n", n1, n2);
+
+    return 0;
+}
+
+void buscador();
--- 4. Ejercicio_unidad5/ejercicio14.c
+++ 4. Ejercicio_unidad5/ejercicio14.c
@@ -1,3 +1,25 @@
 /*
 14. Implementar una función que calcule el área de un círculo con un radio ingresado por el usuario.
 */
+
+#include <stdio.h>
+float calculadora(int radio);
+
+int main(){   
+    int radio;
+    printf("Ingrese el radio: "); //Solicito el radio
+    scanf("%d",&radio);
+
+    float area = calculadora(radio); //Solicito el area de la funcion calculadora
+
+    printf("El area del circulo es: %.4f", area); //Imprimo el area calculada con 4 decimales
+    return 0;
+}
+
+float calculadora(int radio){
+    float pi = 3.14159;
+    float area;
+
+    area = pi * radio*radio; //Calculo el area usando la formula pi * radio^2 
+    return area; // Retorno el area calculada
+}
