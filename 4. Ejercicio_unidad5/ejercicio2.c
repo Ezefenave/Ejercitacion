0952cd18e261850a1b171a8e12b9c34328d65354
ezefenave <ezefenave2209@gmail.com>
2024-05-14 12:54:07 -0300

    Carga

--- 4. Ejercicio_unidad5/ejercicio2.c
+++ 4. Ejercicio_unidad5/ejercicio2.c
@@ -5,10 +5,35 @@
 */
 
 #include <stdio.h>
+int receptor(); //Prototipo de la función de la función
 
 int main()
 {
+    int numero = 0;
+    numero = receptor(); //Llamado de la función
+
+        if (numero == 0){
+            int i;
+            for (i = 15; i <= 50; i++) {
+                printf("%d ", i); // Imprimir del 15 al 50
+            }
+        }else if(numero == 1){
+            int i;
+            for (i = 51; i <= 100; i++) {
+                printf("%d ", i); // Imprimir del 51 al 100
+        }
+        }else{
+            printf("El numero ingresado debe ser 0 o 1"); //
+            }
+
     return 0;
 }
 
+int receptor(){
+    int numero;
 
+    printf("Ingrese un numero 0 o 1: ");
+    scanf("%d", &numero);
+
+    return numero; //Retorno del numero
+}
