0fabd404cbe6297adde7199da6595f3363f7aeeb
ezefenave <ezefenave2209@gmail.com>
2024-05-14 13:47:37 -0300

    Orden

--- 4. Ejercicio_unidad5/ejercicio7.c
+++ 4. Ejercicio_unidad5/ejercicio7.c
@@ -1,3 +1,19 @@
 /*
 7. Implementar una función que imprima letras del alfabeto en minúsculas.
 */
+
+#include <stdio.h>
+void impresor();
+
+int main(){
+    printf("El alfabeto en mayuscula es: "); // Impresion del alfabeto
+    impresor(); //Llamado a la función
+    return 0;
+}
+
+void impresor(){ //Ya que no tiene retorno debe ser un VOID
+    int i;
+    for (i = 97; i <=122; i++){
+        printf("%c. ", i); //Imprimir el alfabeto en minuscula por su codigo ASCII 
+    }
+}
