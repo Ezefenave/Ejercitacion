ab5e2caa2b2988e33e1733747a736119b2924826
ezefenave <ezefenave2209@gmail.com>
2024-05-14 13:45:54 -0300

    Carga

--- 4. Ejercicio_unidad5/ejercicio6.c
+++ 4. Ejercicio_unidad5/ejercicio6.c
@@ -1,3 +1,24 @@
 /*
 6. Implementar una función que reciba un carácter y muestre su valor en ASCII.
 */
+
+#include <stdio.h>
+char convertidor();
+
+int main()
+{
+    char caracter;
+    caracter = convertidor();
+
+    printf("El valor ASCII del numero ingresado es: %d ", caracter);
+    return 0;
+}
+
+char convertidor(){
+    char caracter;
+
+    printf("Ingrese un caracter: ");
+    scanf("%c",&caracter);
+
+    return caracter;
+}
