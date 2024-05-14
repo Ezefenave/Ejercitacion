75d2c872fb1bb4af0cb3de44fb6387da2b17cc4a
ezefenave <ezefenave2209@gmail.com>
2024-05-14 14:30:09 -0300

    Carga

--- 4. Ejercicio_unidad5/ejercicio10.c
+++ 4. Ejercicio_unidad5/ejercicio10.c
@@ -1,3 +1,23 @@
 /*
 10. Implementar una función que valide un número ingresado si es par o impar.
 */
+
+#include <stdio.h>
+void validador();
+
+int main(){
+    int numero;
+    printf("Ingrese el numero que desea saber si es par o impar: "); 
+    scanf("%d",&numero);
+
+    validador(numero); //Llamado a función
+    return 0;
+}
+
+void validador(int numero){  //Función validadora
+    if (numero / 2 == 0){
+        printf("El numero ingresado es impar");
+    }else{
+        printf("El numero ingresado es par");
+    }
+}
--- 4. Ejercicio_unidad5/ejercicio9.c
+++ 4. Ejercicio_unidad5/ejercicio9.c
@@ -2,3 +2,34 @@
 9. Implementar una función que calcule el interés compuesto para una cantidad de dinero, con tasas y
 períodos por mes.
 */
+
+#include <stdio.h>
+float calculador();
+
+int main()
+{
+    float cantidadDinero;
+    float tasas;
+    int periodoMes;
+
+    printf("Ingrese el dinero inicial: ");
+    scanf("%lf", &cantidadDinero);
+    printf("Ingrese la tasa de interes: ");
+    scanf("%lf", &tasas);
+    printf("Ingrese el perdiodo por mes: ");
+    scanf("%d", &periodoMes);
+
+
+    return 0;
+}
+
+float calculador(){
+    float cantidadDinero;
+    float tasas;
+    int periodoMes;
+
+    
+
+    
+
+}
