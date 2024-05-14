c9ac6210bb901d12582e1c98e07ad4c6c658db95
ezefenave <ezefenave2209@gmail.com>
2024-05-14 13:18:32 -0300

    Carga

--- 4. Ejercicio_unidad5/ejercicio3.c
+++ 4. Ejercicio_unidad5/ejercicio3.c
@@ -1,3 +1,20 @@
 /*
 3. Implementar una función que imprima las letras del alfabeto en mayúsculas (Ver tabla ASCII).
 */
+
+#include <stdio.h>
+void impresor();
+
+int main(){
+    printf("El alfabeto en mayuscula es: "); // Impresion del alfabeto
+    impresor(); //Llamado a la función
+    return 0;
+}
+
+void impresor(){ //Ya que no tiene retorno debe ser un VOID
+    int i;
+    for (i = 65; i <=90; i++){
+        printf("%c. ", i); //Imprimir el alfabeto en mayuscula por su codigo ASCII 
+    }
+}
+
--- 4. Ejercicio_unidad5/ejercicio4.c
+++ 4. Ejercicio_unidad5/ejercicio4.c
@@ -3,3 +3,29 @@
 parámetro al cero.
 Ejemplo si ingresa el 5 imprime (5, 4, 3, 2, 1, 0);
 */
+
+#include <stdio.h>
+int receptor();
+
+int main(){
+    int numero = 0;
+
+    numero = receptor();
+    
+    printf("El numero desciende hasta el 0 de la siguiente manera: ");
+
+    int i;
+    for (i = numero; i >= 0; i--){
+        printf("%d, ", i);
+    }
+    return 0;
+}
+
+int receptor(){
+    int numero;
+
+    printf("Ingrese un numero: "); //Pedido al usuario del número
+    scanf("%d", &numero);
+
+    return numero; //Retorno del numero
+}
