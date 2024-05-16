c5d840c6298fc025f62df4dc57b640e344d70d50
ezefenave <ezefenave2209@gmail.com>
2024-05-16 13:28:11 -0300

    Carga

--- 5.ejercicios_unidad6/ejercicio1.c
+++ 5.ejercicios_unidad6/ejercicio1.c
@@ -4,25 +4,36 @@ ingresado por el usuario. Al finalizar mostrar el promedio de los valores.
 */
 
 #include <stdio.h>
-
-#define tam 4
+#define tam 30
 
 int main (){
+    //Declaro las variables
 int vectorPar [tam];
-int acumulador = 0;
+int numeroInicial;
+float acumulador = 0;
 float promedio;
-int contador;
 
-    for(int i = 0; i < tam; i++){
-        printf("Ingrese un numero para ingresar en el vector (recuerda que solo se almacenaran los numeros pares): "); //Solicito los numeros al usuario
-        scanf("%d",&vectorPar[i]); // Ingreso los numeros en el vector
+    printf("Ingrese el numero que deseas poner como limite: "); //Solicito el número al usuario
+    scanf("%d",&numeroInicial); 
 
-        if(vectorPar[i] % 2 == 0){ // Compruebo si el número ingresado es par o no
-            contador++; 
-            acumulador += vectorPar[i]; // Si es par lo sumo al acumulador para luego calcular el promedio
-        }   
-    }  
+    //Si el numero que me ingresa es impar le sumo 1 para que se incremente de a numeros pares
+    if (numeroInicial % 2 != 0) { 
+        numeroInicial++;
+    }
+    //Genero 30 numeros para llenar el vector
+    for(int i = 0; i <tam; i++){
+        vectorPar[i] = numeroInicial + 2 * i; //De esta manera logro que se almacenen en el vector los numeros pares. 
+        acumulador += vectorPar[i]; // Sumo los numeros generados al acumulador
+    }
 
-    promedio = (float)acumulador / contador; //De esta manera calculo el promedio
-    printf("El promedio de los valores dentro del vector es igual a: %.2f", promedio);
+    printf("El vector queda de la siguiente manera:\n");
+    for(int i = 0; i < tam; i++){ //Imprimo el vector 
+        printf("%d ", vectorPar[i]);
+    }
+
+    // Calculo el promedio de esta manera ya que el tamaño del vector es fijo
+    promedio = acumulador / tam; 
+    printf("\nEl promedio de los valores dentro del vector es igual a: %.2f", promedio);
+    
+    return 0;
 }
--- 5.ejercicios_unidad6/ejercicio2.c
+++ 5.ejercicios_unidad6/ejercicio2.c
@@ -1,3 +1,40 @@
 /*
 2. Modifique el ejercicio anterior, para mostrar los valores que sean mayores que el promedio.
 */
+
+#include <stdio.h>
+#define tam 30
+
+int main (){
+    //Declaro las variables
+int vectorPar [tam];
+int numeroInicial;
+float acumulador = 0;
+float promedio;
+
+    printf("Ingrese el numero que deseas poner como limite: "); //Solicito el número al usuario
+    scanf("%d",&numeroInicial); 
+
+    //Si el numero que me ingresa es impar le sumo 1 para que se incremente de a numeros pares
+    if (numeroInicial % 2 != 0) { 
+        numeroInicial++;
+    }
+    //Genero 30 numeros para llenar el vector
+    for(int i = 0; i <tam; i++){
+        vectorPar[i] = numeroInicial + 2 * i; //De esta manera logro que se almacenen en el vector los numeros pares. 
+        acumulador += vectorPar[i]; // Sumo los numeros generados al acumulador
+    }
+
+    /// Calculo el promedio de esta manera ya que el tamaño del vector es fijo
+    promedio = acumulador / tam; 
+    printf("El promedio de los valores dentro del vector es igual a: %.2f\n", promedio);
+
+    //Imprimo los numeros mayores al promedio que se encuentran en el vector
+    printf("Los numeros pares dentro del vector que son mayores al promedio son:\n");
+    for(int i = 0; i < tam; i++){ 
+        if(vectorPar[i] > promedio) { 
+            printf("%d,", vectorPar[i]); // Muestra el valor si es mayor que el promedio
+        }
+    }
+    return 0;
+}
