c5cf4cb45bce98dafaf05e9f936fa16b157f16d9
ezefenave <ezefenave2209@gmail.com>
2024-05-19 14:49:32 -0300

    Carga

--- 5.ejercicios_unidad6/ejercicio4.c
+++ 5.ejercicios_unidad6/ejercicio4.c
@@ -2,3 +2,33 @@
 4. Un programa que pida al usuario 10 números enteros y calcule (y muestre) cuál o cuáles es el
 mayor de ellos indicando la posición en que fue o fueron encontrados.
 */
+
+#include <stdio.h>
+#define tam 10 //Defino el tamaño de mi vector
+    
+int main(){
+    int vectorN[10];
+    int banderaMayor = 0;
+    int mayor;
+    int indiceMayor = 0;
+
+    printf("Ingrese numeros:\n");
+    
+    for (int i = 0; i < tam; i++){ //Solicito al usuario que me ingrese los numeros que busca almacenar en el vector indicandoles el indice
+        printf("Elemento %d: ", i);
+        scanf("%d",&vectorN[i]);
+    }
+        mayor = vectorN[0]; //Determino al mayor como el primer elemento de mi vector
+
+    for (int i = 1; i < tam; i ++){ //Inicio el bucle para recorrer el vector
+        if(vectorN[i] > mayor){ //Si el numero que lee mi bucle es mayor al valor que contiene almacenado como mayor lo sustituye
+        mayor = vectorN[i] ; //Cambio el valor del mayor para mi vector
+        indiceMayor = i; //De esta manera indico en que posicion del indice fue encontrado el numero mayor
+        banderaMayor = 1; //Cambio el valor de mi bandera 
+        }
+    }
+
+    printf("El numero mayor encontrado entre los numeros ingresados es: %d. El cual fue encontrado en la posicion %d", mayor, indiceMayor);
+
+    return 0;
+}
--- 5.ejercicios_unidad6/ejercicio5.c
+++ 5.ejercicios_unidad6/ejercicio5.c
@@ -5,3 +5,38 @@ b- Eliminar un elemento y todas sus repeticiones.
 c- Contar las apariciones de un número en una columna determinada.
 d- Terminar
 */
+#include <stdio.h>
+#define tam 10
+
+int main(){
+int vector[tam];
+int opcion;
+
+
+    printf("Ingrese el numero correspondiente a la operacion que desea realizar:\n");
+    printf("1- Añadir un elemento.\n");
+    printf("2- Eliminar un elemento y todas sus repeticiones.\n");
+    printf("3-Contar las apariciones de un numero en una columna determinada.\n");
+    printf("4-Terminar\n");
+    scanf("%d",&opcion);
+
+    switch (opcion){
+    case 1:
+        printf("Ingrese el numero que desea añadir al vector: ");
+        scanf("%d, vector");
+        break;
+    case 2: 
+        printf("Ingrese el numero que desea eliminar del vector: ");
+        scanf("%d, vector");
+    case 3:
+        printf("Ingrese el numero que desea contar: ");
+        scanf("%d, vector");
+    case 4:
+        printf("Fin del programa");
+    default:
+        break;
+    }
+
+    
+    return 0;
+}
